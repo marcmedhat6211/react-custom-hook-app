80b13ef0d481b359f068545ff7da632527222d9f
Marc Medhat <marcmedhat6211@gmail.com>
2022-05-28 10:31:33 +0200

    react custom hook app

---
+++ src/hooks/use-counter.js
@@ -0,0 +1,25 @@
+import { useState, useEffect } from "react";
+
+/**
+ * Custom Hooks:
+ *    The function have to start with the use keyword for react to realize that it is a hook
+ */
+const useCounter = (forwards = true) => {
+  const [counter, setCounter] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      if (forwards) {
+        setCounter((prevCounter) => prevCounter + 1);
+      } else {
+        setCounter((prevCounter) => prevCounter - 1);
+      }
+    }, 1000);
+
+    return () => clearInterval(interval);
+  }, [forwards]);
+
+  return counter;
+};
+
+export default useCounter;
